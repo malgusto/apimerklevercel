5596b0875fcaca86bd9afffcb2119699354cddf8
malgusto <malgusto@gmail.com>
2024-03-10 16:30:38 +0100

    Update main.py111

--- main.py
+++ main.py
@@ -141,3 +141,6 @@ async def predict(url: str = Query(..., min_length=30, max_length=500)):
     result = predecir_articulo(url)
     # return {"url": url,"titulo":result[0], "etikmeans": result[1].upper(),"etildag":  result[2].upper(),"etildask":  result[3].upper(),"etilsag":  result[4].upper(),"screenshot": result[5],"sobretexto": result[6]}
     return {"url": url,"titulo":result[0], "etikmeans": result[1].upper(),"etildag":  result[2].upper(),"etildask":  result[3].upper(),"etilsag":  result[4].upper(),"screenshot": "1","sobretexto": result[6]}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
